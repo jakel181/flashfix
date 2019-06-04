79fb5e49d817e7a80bfdf84d5fec9f5ff2d3514d
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2019-06-04 16:01:34 -0400

    Create generateLink.js

---
+++ generateLink.js
@@ -0,0 +1,18 @@
+function create(code,name,parentID) {
+  var a = document.createElement("a");
+  a.textContent = name;
+  a.href = 'javascript:innerHTML=document.getElementsByTagName("head")[0].innerHTML+document.getElementsByTagName("body")[0].innerHTML+"<scr'+'ipt>'+code+'</scr'+'ipt>"';
+  document.getElementById(parentID).appendChild(a);
+}
+function flashFix() {
+  var a = document.createElement('a');
+  a.setAttribute('href','https://get.adobe.com/flashplayer');
+  a.click();
+}
+function getFunctionContent(name) {
+  var f = window[name];
+  var txt = f.toString();
+  var t = txt.slice(txt.indexOf("{"),txt.lastIndexOf("}"));
+  return t;
+}
+create(getFunctionContent('flashFix'),"Enable Flash","link");
